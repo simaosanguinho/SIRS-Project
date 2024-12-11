523146548edd6488128d9f2bcee3810437bd04c1
Rafael Girão <rafael.s.girao@tecnico.ulisboa.pt>
2024-12-11 15:56:45 +0000

    chore: add treefmt & git-hooks

--- flake.nix
+++ flake.nix
@@ -6,6 +6,15 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
@@ -17,7 +26,8 @@
         # 1. Add foo to inputs
         # 2. Add foo as a parameter to the outputs function
         # 3. Add here: foo.flakeModule
-
+        inputs.git-hooks-nix.flakeModule
+        inputs.treefmt-nix.flakeModule
       ];
       systems = [
         "x86_64-linux"
@@ -57,9 +67,47 @@
               ]))
               pkgs.openssl
             ];
+              shellHook = ''
+                # export DEBUG=1
+                ${config.pre-commit.installationScript}
+              '';
+
           };
 
+          pre-commit = {
+            check.enable = true;
+            settings.hooks = {
+              actionlint.enable = true;
+             # treefmt.enable = true;
+              commitizen = {
+                enable = false;
+                description = "Check whether the current commit message follows commiting rules. Allow empty commit messages by default, because they typically indicate to Git that the commit should be aborted.";
+                entry = "${pkgs.commitizen}/bin/cz check --allow-abort --commit-msg-file";
+                stages = [ "commit-msg" ];
+              };
+              gitleaks = {
+                enable = true;
+                name = "gitleaks";
+                description = "Prevents commiting secrets";
+                entry = "${pkgs.gitleaks}/bin/gitleaks protect --verbose --redact --staged";
+                pass_filenames = false;
+              };
+            };
+          };
+          treefmt.projectRootFile = ./flake.nix;
+          treefmt.programs = {
+            nixpkgs-fmt.enable = true;
+            shfmt.enable = true;
+            mdformat.enable = true;
+            deadnix.enable = true;
+            statix.enable = true;
+            ruff-check.enable = true;
+            ruff-format.enable = true;
+          };
         };
+  
+
+      
       flake = {
         # The usual flake attributes can be defined here, including system-
         # agnostic ones like nixosModule and system-enumerating ones, although
