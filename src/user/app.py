4d570a10bd25e7c6ec3a8849471ce7434b728328
pereira0x <jose.a.pereira03@gmail.com>
2025-01-01 20:24:17 +0000

    fix(user): import PKI from cryptolib

--- src/user/app.py
+++ src/user/app.py
@@ -8,6 +8,8 @@ import requests
 import cryptolib
 import json
 import os
+from cryptolib import PKI
+
 
 PROJECT_ROOT = os.getenv("PROJECT_ROOT", "../../")
 KEY_STORE = os.getenv("KEY_STORE", f"{PROJECT_ROOT}/key_store")
